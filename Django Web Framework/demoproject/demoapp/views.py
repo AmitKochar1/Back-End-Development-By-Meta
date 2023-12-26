5cfcc25ebdc31bbd2e658083b8a1198840fb24a1
AmitKochar1 <amitkochar91@gmail.com>
2023-12-26 17:23:10 +0700

    Django - Requests and URLs module

--- Django Web Framework/demoproject/demoapp/views.py
+++ Django Web Framework/demoproject/demoapp/views.py
@@ -3,25 +3,36 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home(request):
-    path = request.path
-    scheme= request.scheme
-    method = request.method
-    address = request.META["REMOTE_ADDR"]
-    user_agent = request.META['HTTP_USER_AGENT']
-    path_info = request.path_info
+def menuitems(request, dish):
+    items = {
+        "pasta": "Pasta is a type of noodle made from combination of wheat and grain",
+        "falafel": "Falafel are deep fried patties",
+        "cheesecake": "Cheesecake is a type of dessert",
+    }
 
-    response = HttpResponse()
-    response.headers['age'] = '20'
+    description = items[dish]
 
-    msg = f"""<br>
-        <br>Path:{path}
-        <br>Address: {address}
-        <br>Scheme: {scheme}
-        <br>Method: {method}
-        <br>User agent: {user_agent}
-        <br>Path info: {path_info}
-        <br>Response Header: {response.headers}
-    """
-    return HttpResponse(msg, content_type='text/html', charset='utf-8')
+    return HttpResponse(f'<h1>{dish}</h1>' + description)
+
+# def home(request):
+#     path = request.path
+#     scheme= request.scheme
+#     method = request.method
+#     address = request.META["REMOTE_ADDR"]
+#     user_agent = request.META['HTTP_USER_AGENT']
+#     path_info = request.path_info
+
+#     response = HttpResponse()
+#     response.headers['age'] = '20'
+
+#     msg = f"""<br>
+#         <br>Path:{path}
+#         <br>Address: {address}
+#         <br>Scheme: {scheme}
+#         <br>Method: {method}
+#         <br>User agent: {user_agent}
+#         <br>Path info: {path_info}
+#         <br>Response Header: {response.headers}
+#     """
+#     return HttpResponse(msg, content_type='text/html', charset='utf-8')
 
