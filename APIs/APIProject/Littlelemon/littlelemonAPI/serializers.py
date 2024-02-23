1486ecee68c60b11ad745dbdbf2ae4e250f686a2
AmitKochar1 <amitkochar91@gmail.com>
2024-02-23 13:56:49 +0530

    API - Serializer

--- APIs/APIProject/Littlelemon/littlelemonAPI/serializers.py
+++ APIs/APIProject/Littlelemon/littlelemonAPI/serializers.py
@@ -1,14 +1,71 @@
 from rest_framework import serializers
-from .models import Menuitem
-from decimal import Decimal
+from .models import Category, Menuitem, Cart, Order, OrderItem
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class MenuItemSerializer(serializers.ModelSerializer):
-    stock = serializers.IntegerField(source = 'inventory')
-    price_after_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    
+    class Meta():
+        model = Menuitem
+        fields = ['id','title', 'price', 'featured', 'category']
+        depth = 1
+        
+class CategorySerializer(serializers.Serializer):
+    class Meta:
+        model = Category
+        fields = ['slug']
+        
+class ManagerSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+        
+class CartHelpSerializer(serializers.Serializer):
     class Meta:
         model = Menuitem
-        fields = ['id', 'title', 'stock','price']
+        fields = ['id', 'title', 'user']
         
-    def calculateTax(self, product:Menuitem):
-        return product.price * Decimal(1.1) 
+class CartSerializer(serializers.Serializer):
+    menuitem = CartHelpSerializer()
+    class Meta:
+        model = Cart
+        fields = ['menuitem', 'quantity', 'price']
+
+class CartAddSerializer(serializers.Serializer):
+    class Meta:
+        model = Cart
+        fields = ['menuitem', 'quantity']
+        extra_kwargs = {
+            'quantity': {'min_value':1},
+        }
+        
+class CartRemoveSerializer(serializers.Serializer):
+    class Meta:
+        model = Cart
+        fields = ['menuitem']
+        
+class UserSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields=['username']
+        
+class OrderSerializer(serializers.Serializer):
+    user = UserSerializer()
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'delivery_crew', 'status', 'total', 'date']
+        
+class SingleHelperSerializer(serializers.Serializer):
+    class Meta:
+        model = Menuitem
+        fields = ['title', 'price']
+        
+class SingleOrderSerializer(serializers.Serializer):
+    menuitem = SingleHelperSerializer()
+    class Meta:
+        model = OrderItem
+        fields = ['menuitem', 'quantity']
+        
+class OrderPutSerializer(serializers.Serializer):
+    class Meta:
+        model = Order
+        fields = ['delivery_crew']
--- APIs/APIProject/Littlelemon/littlelemonAPI/urls.py
+++ APIs/APIProject/Littlelemon/littlelemonAPI/urls.py
@@ -3,11 +3,11 @@ from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('menu-items/', views.menu_items),
-    path('single-item', views.single_item), 
-    path('secret/', views.secret),
-    path('api-token-auth/', obtain_auth_token),
-    path('manager-view/', views.manager_view),
-    path('throttle-check/', views.throttle_check),
-    path('throttle-check-auth/', views.throttle_check_auth)
+    # path('menu-items/', views.menu_items),
+    # path('single-item', views.single_item), 
+    # path('secret/', views.secret),
+    # path('api-token-auth/', obtain_auth_token),
+    # path('manager-view/', views.manager_view),
+    # path('throttle-check/', views.throttle_check),
+    # path('throttle-check-auth/', views.throttle_check_auth)
 ]
