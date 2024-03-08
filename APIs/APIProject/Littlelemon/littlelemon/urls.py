b37568e2553abbaa28308030c3c56d1d60700978
AmitKochar1 <amitkochar91@gmail.com>
2024-03-08 17:29:31 +0530

    API module completed

--- APIs/APIProject/Littlelemon/littlelemon/urls.py
+++ APIs/APIProject/Littlelemon/littlelemon/urls.py
@@ -20,5 +20,16 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('littlelemonAPI.urls')),
-    
 ]
+
+# as per meta
+
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/',include('LittleLemonDRF.urls')),
+#     path('auth/', include('djoser.urls')),
+#     path('auth/', include('djoser.urls.authtoken')),
+# ]
